d670f9572cfafe1688448a290eb1e4bfdbcc4193
cammywan <cammywan0218@gmail.com>
2020-02-15 15:54:06 -0500

    Update project2.js

--- project2.js
+++ project2.js
@@ -1,29 +1,4 @@
 $(document).ready(function() {
-  // Toggle the receive spam checkbox
-  $("#receivespambutton").click(function() {
-
-    if($($("#receivespambutton").children()[0]).attr("src")=="pics/unchecked.png") {
-      $($("#receivespambutton").children()[0]).attr("src","pics/checked.png");
-      $("#spamyn").val("y");
-    } else {
-      $($("#receivespambutton").children()[0]).attr("src","pics/unchecked.png");
-      $("#spamyn").val("n");
-    }
-  });
-
-
-  // Play video
-  $("#videoplayer").mousedown(function() {
-
-    if($($("#videoplayer").children()[0]).attr("src")=="pics/play.png") {
-      $($("#videoplayer").children()[0]).attr("src","pics/pause.jpg");
-      $("#thevideo")[0].play();
-    } else {
-      $($("#videoplayer").children()[0]).attr("src","pics/play.png");
-      $("#thevideo")[0].pause();
-    }
-  });
-
   // Form validation
   $("#signupbutton").click(function() {
     if($("#fn").val()=="" || $("#mi").val()=="" || $("#ln").val()=="") {
